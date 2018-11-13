2ca1d434b49c8b8541231e00809eb1845f1e5986
grossmanm <malcolmcgrossman@gmail.com>
2018-11-13 15:30:07 -0600

    should work now

--- RNN/temp_condenser.py
+++ RNN/temp_condenser.py
@@ -7,28 +7,26 @@ work = os.getcwd()
 with open("train.txt", "w") as f:
     for file in os.listdir("train"):
         with open(work + '/train/' +  file, "r") as f1:
-            sents = []
-            lines = f1.readlines()
-            for line in lines:
-                sents += re.split(r'([.])', line)
-            for sent in sents:
+            text = f1.read()
+            text = text.strip()
+            text = re.split('.', line)
+            for sent in text:
                 f.write(sent + "\n")
 
 with open("valid.txt", "w") as f:
     for file in os.listdir("valid"):
         with open(work + '/valid/' + file, "r") as f1:
-            sents = []
-            lines = f1.readlines()
-            for line in lines:
-                sents += re.split(r'([.])', line)
-            for sent in sents:
+            text = f1.read()
+            text = text.strip()
+            text = re.split('.', line)
+            for sent in text:
                 f.write(sent + "\n")
+
 with open(work + "test.txt", "w") as f:
     for file in os.listdir("test"):
         with open(workd + '/test/' + file, "r") as f1:
-            sents = []
-            lines = f1.readlines()
-            for line in lines:
-                sents += re.split(r'([.])', line)
-            for sent in sents:
+            text = f1.read()
+            text = text.strip()
+            text = re.split('.', line)
+            for sent in text:
                 f.write(sent + "\n")
