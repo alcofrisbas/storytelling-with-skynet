97a42c5d1efb3520bad4e9da5e3671f049f2ea5b
grossmanm <malcolmcgrossman@gmail.com>
2018-11-13 15:11:56 -0600

    condensing into one file

---
+++ RNN/temp_condenser.py
@@ -0,0 +1,34 @@
+import re
+import os
+
+os.chdir("../webscrape-gutenberg")
+work = os.getcwd()
+
+with open("train.txt", "w") as f:
+    for file in os.listdir("train"):
+        with open(file, "r") as f1:
+            sents = []
+            lines = f1.readlines()
+            for line in lines:
+                sents += re.split(r'([.])', line)
+            for sent in sents:
+                f.write(sent + "\n")
+
+with open("valid.txt", "w") as f:
+    for file in os.listdir("valid"):
+        with open(file, "r") as f1:
+            sents = []
+            lines = f1.readlines()
+            for line in lines:
+                sents += re.split(r'([.])', line)
+            for sent in sents:
+                f.write(sent + "\n")
+with open("test.txt", "w") as f:
+    for file in os.listdir("test"):
+        with open(file, "r") as f1:
+            sents = []
+            lines = f1.readlines()
+            for line in lines:
+                sents += re.split(r'([.])', line)
+            for sent in sents:
+                f.write(sent + "\n")
