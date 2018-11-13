a8adf586a55d9726fe694620a0a2a996a64e1e2c
grossmanm <malcolmcgrossman@gmail.com>
2018-11-13 16:22:21 -0600

    read binary

--- RNN/temp_condenser.py
+++ RNN/temp_condenser.py
@@ -4,27 +4,27 @@ import os
 os.chdir("../webscrape-gutenberg/")
 work = os.getcwd()
 
-with open("train.txt", "w") as f:
+with open("train.txt", "wb") as f:
     for file in os.listdir("train"):
-        with open(work + '/train/' +  file, "r") as f1:
+        with open(work + '/train/' +  file, "rb") as f1:
             text = f1.read()
             text.replace('^M','')
             print(text)
             for sent in text:
                 f.write(sent + "\n")
 
-with open("valid.txt", "w") as f:
+with open("valid.txt", "wb") as f:
     for file in os.listdir("valid"):
-        with open(work + '/valid/' + file, "r") as f1:
+        with open(work + '/valid/' + file, "rb") as f1:
             text = f1.read()
             text = text.strip()
             text = re.split('.', text)
             for sent in text:
                 f.write(sent + "\n")
 
-with open(work + "test.txt", "w") as f:
+with open(work + "test.txt", "wb") as f:
     for file in os.listdir("test"):
-        with open(work + '/test/' + file, "r") as f1:
+        with open(work + '/test/' + file, "rb") as f1:
             text = f1.read()
             text = text.strip()
             text = re.split('.', text)
