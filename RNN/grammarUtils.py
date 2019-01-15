4597a2b4cd0d7c075804add37e8bb8e54aee081a
alcofrisbas <bgreene231@gmail.com>
2019-01-15 14:05:37 -0600

    comments in grammarutils

--- RNN/grammarUtils.py
+++ RNN/grammarUtils.py
@@ -2,11 +2,19 @@ from nltk import word_tokenize, pos_tag
 from random import randrange as rrange
 import time
 
+"""
+given a sentence, returns
+a list containing the nltk
+"""
 def structure_from_sent(sentence):
     text = word_tokenize(sentence)
     text = pos_tag(text)
     return [i[1] for i in text]
 
+"""
+given our list of structures, returns
+a random sentence structure 
+"""
 def pick_structure(max=42068):
     start = time.time()
     with open("data/templates.csv", "r") as r:
