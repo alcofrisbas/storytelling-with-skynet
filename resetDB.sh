f70e9c50bd9a237e53403b799dfecc50e9d3bc88
alcofrisbas <bgreene231@gmail.com>
2019-01-20 15:09:33 -0600

    commented in db and saves for clarity

--- resetDB.sh
+++ resetDB.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# run this when you have changed the structure/
+# added models to our code
+
 rm writerbot-webapp/db.sqlite3
 venv/bin/python writerbot-webapp/manage.py migrate --run-syncdb
