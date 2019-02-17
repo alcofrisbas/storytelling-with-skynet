f7044412c8eddb834d330a5831cb800aad0431c5
alcofrisbas <bgreene231@gmail.com>
2019-02-17 15:20:11 -0600

    more evo

--- evolution/evo.py
+++ evolution/evo.py
@@ -12,15 +12,13 @@ from utils import *
 
 
 class Individual:
-    def __init__(self, labels, values, code="", type_markers=[]):
+    def __init__(self, labels, values, code="", type_markers=[], exempt={}):
         self.labels = labels
         self.values = values
         self.code = code
         self.type_markers = type_markers
         if self.code and len(self.values) == 0:
-            print("decodeing")
             self.decode()
-            print("done decoding")
         else:
             self.encode()
 
@@ -43,9 +41,13 @@ class Individual:
                 self.values.append(bin_to_int(self.code[start: end]))
             elif t == "float":
                 self.values.append(bin_to_float(self.code[start: end]))
-            else:
-                print(t)
-        print(self.values)
+
+    def attributes(self):
+        d = {}
+        for i in range(len(self.values)):
+            d[self.labels[i]] = self.values[i]
+        d.update(exempt)
+        return d
 
 def mutate(s):
     prob = 1.0/len(s)
@@ -114,13 +116,14 @@ def save_individuals(fname,d):
 if __name__ == '__main__':
     labels = ["init_scale","learning_rate", "max_grad_norm", "num_layers",
             "num_steps", "hidden_size", "max_epoch", "max_max_epoch", "keep_prob",
-            "lr_decay", "batch_size", "vocab_size"]
+            "lr_decay", "batch_size"]
 
-    values1 = [0.1, 1.0, 5, 2, 20, 200, 4, 20, 1.0, 0.5, 1, 10011]
-    values2 = [0.05, 1.0, 5, 2, 35, 650, 6, 39, 0.8, 0.8, 1, 10011]
-    i1 = Individual(labels, values2)
-    i2 = Individual(labels, values1)
+    values1 = [0.1, 1.0, 5, 2, 20, 200, 4, 1, 1.0, 0.5, 1]
+    values2 = [0.05, 1.0, 5, 2, 35, 650, 6, 1, 0.8, 0.8, 1]
+    exempt = {"vocab_size":10011}
+    i1 = Individual(labels, values2, exempt= exempt)
+    i2 = Individual(labels, values1,exempt = exempt)
 
-    i3, i4 = crossover(i1,i2, k=250)
+    i3, i4 = crossover(i1,i2, k=300)
     for i in range(len(i3.labels)):
-        print("{}:\t{}".format( i3.labels[i], str(i3.values[i])))
+        print("{}:\t{:.5}\t{:.5}".format( i3.labels[i], str(i4.values[i]), str(i3.values[i])))
