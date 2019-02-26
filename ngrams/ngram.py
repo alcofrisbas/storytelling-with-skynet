cb339072c1b6f3abc69d6c04ef8320bce35e3f16
alcofrisbas <bgreene231@gmail.com>
2019-02-26 12:53:38 -0600

    ngrams working, but need improvement.

--- ngrams/ngram.py
+++ ngrams/ngram.py
@@ -1,4 +1,6 @@
 import numpy as np
+from nltk.tokenize import sent_tokenize
+
 class Trie:
     def __init__(self, key):
         self.key = key
@@ -25,80 +27,78 @@ def add(root, l):
     node.done = True
 
 def find_prefix(root, prefix):
-    """
-    Check and return
-      1. If the prefix exsists in any of the words we added so far
-      2. If yes then how may words actually have the prefix
-    """
     node = root
-    # If the root node has no children, then return False.
-    # Because it means we are trying to search in an empty trie
     if not root.children:
         return False, 0
     for word in prefix:
         not_found = True
-        # Search through all the children of the present `node`
         for child in node.children:
             if child.key == word:
-                # We found the char existing in the child.
                 not_found = False
-                # Assign node as the child containing the char and break
                 node = child
                 break
-        # Return False anyway when we did not find a char.
         if not_found:
             return False, 0
-    # Well, we are here means we have found the prefix. Return true to indicate that
-    # And also the counter of the last node. This indicates how many words have this
-    # prefix
+
     return True, node
 
 def predict_next(root, prefix):
-    _, parent = find_prefix(root, prefix)
-    if _:
-        childs = [child.key for child in parent.children]
-        total = sum([child.payload for child in parent.children])
-        weights = [child.payload/total for child in parent.children]
-        print (sum(weights))
-        next = np.random.choice(childs, p=weights)
-        return next
+    found = False
+    while not found:
+        print(prefix)
+        found, parent = find_prefix(root, prefix)
+        if found:
+            childs = [child.key for child in parent.children]
+            total = sum([child.payload for child in parent.children])
+            weights = [child.payload/total for child in parent.children]
+            #print (sum(weights))
+            next = np.random.choice(childs, p=weights)
+            return next
+        prefix.pop(0)
     return
 
-def corpus_to_dict(fname, n):
-    with open(fname, "r") as r:
+def process_data(fname):
+    with open(fname, 'r') as r:
         text = r.read()
-    d = {}
-    words = text.split()
-    for i in range(len(words)):
-        pass
+    sList = sent_tokenize(text)
+    sList = [i[:-1]+" STOP" for i in sList]
 
-def train(fname, n=4):
+    with open(fname+".tkn" , 'w') as w:
+        w.write(" ".join(sList))
+
+
+def train(fname, n=5):
     with open(fname) as r:
         s = r.read()
-    words = s.lower().split()
+    words = s.split()
+    words = [w.lower() if w != 'STOP' else w for w in words ]
     root = Trie("*")
     for i in range(len(words)-n):
         add(root,words[i:i+n])
-        if i%1000 == 0:
+        if i%2000 == 0:
             print("{} substrings processed".format(str(i)))
 
-        if i > 200000:
+        if i > 50000:
             break
     return root
 
+def generate_sentence(root, sent):
+    sentence = sent.split()
+    for i in range(l):
+        next = predict_next(root, sentence[-m:])
+        sentence.append(next)
+        if sentence[-1] == "STOP":
+            break
+    return " ".join([str(word) for word in sentence[:-1]])
+
 if __name__ == '__main__':
     trie = {}
-    root = train("./ngrams/dickens.txt")
+    process_data("./ngrams/dickens.txt")
+    root = train("./ngrams/dickens.txt.tkn")
 
-    sent = input("enter a sentence: ")
-    l = 20
+    sent = input("enter a sentence: ").lower()
+    l = 200
     m = 3
     while sent != "quit":
-        sentence = sent.split()
-        for i in range(l):
-            next = predict_next(root, sentence[-m:])
-            sentence.append(next)
-            if next[-1] == ".":
-                break
-        print(" ".join([str(word) for word in sentence]))
-        sent = input("enter a sentence: ")
+        print (generate_sentence(root, sent))
+        sent = input("enter a sentence: ").lower()
