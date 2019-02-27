11f29b36182e4a4ae3c37db7345b1fc88e15f9c5
grossmanm <malcolmcgrossman@gmail.com>
2019-02-27 16:59:50 -0600

    runs fine

--- rnn_train.py
+++ rnn_train.py
@@ -13,7 +13,7 @@ def train(training_file, root_path, model_name, n_hidden, min_count, learning_ra
 
     rnn_words.run(learning_rate=learning_rate, training_iters=training_iters,
         n_input=n_input, batch_size=batch_size,n_hidden=n_hidden,
-        path_to_model=path_to_model,model_name=model_name, train=train)
+        path_to_model=root_path,model_name=model_name, train=train)
 
 if __name__ == '__main__':
     train(training_file="simpleRNN/data/train.txt",root_path="simpleRNN/models/",
