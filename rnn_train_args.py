919943f581254c5718dc0481f0bf2c4c1b4e732e
alcofrisbas <bgreene231@gmail.com>
2019-02-28 16:13:00 -0600

    updating cli

---
+++ rnn_train_args.py
@@ -0,0 +1,11 @@
+import argparse
+import sys
+
+def main():
+    parser = argparse.ArgumentParser(description="asdf wtf")
+    
+    args = parser.parse_args(sys.argv[1:])
+
+
+if __name__ == '__main__':
+    main()
