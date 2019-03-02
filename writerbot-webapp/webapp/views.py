b90f28c191d64ddf353aee6edc97129d3d17a644
GalenWQ <galen.wq@gmail.com>
2019-03-02 14:50:53 -0600

    loading seq2seq is broken

--- writerbot-webapp/webapp/views.py
+++ writerbot-webapp/webapp/views.py
@@ -12,7 +12,7 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(sys.path[0]),'simpleRNN'))
 from rnn_words import SimpleRNN
-from rnn_words_seq2seq import SimpleRN as seq2seqRNN
+from rnn_words_seq2seq import SimpleRNN as seq2seqRNN
 import tensorflow as tf
 import random
 from webapp.words import ADJECTIVES, ANIMALS
@@ -41,21 +41,22 @@ class PromptMode(Enum):
 args_dict = {"n_input": 4, "batch_size": 1, "n_hidden": 300, "learning_rate": 0.001, "training_iters": 50000, "training_file": "simpleRNN/data/train.txt"}
 display_step = 1000
 path_to_model = "simpleRNN/models/"
-path_to_seq2seq_model = "simpleRNN/seq2seq_models"
+path_to_seq2seq_model = "simpleRNN/seq2seq_models/"
 model_name = "basic_model"
 
 print("instantiating RNN")
 rnn = SimpleRNN(args_dict, display_step, path_to_model, model_name)
-seq2seq_rnn = seq2seqRNN(args_dict, display_step, path_to_seq2seq_model, model_name)
 print("instantiating saver")
 sess = tf.Session()
-seq2seq_sess = tf.Session()
 saver = tf.train.Saver()
-seq2seq_saver = tf.train.Saver()
 print("loading saved RNN from " + rnn.path_to_model)
 saver.restore(sess, tf.train.latest_checkpoint(rnn.path_to_model))
+
+seq2seq_rnn = seq2seqRNN(args_dict, display_step, path_to_seq2seq_model, model_name, False)
 print("loading saved seq2seqRNN from " + seq2seq_rnn.path_to_model)
-saver.restore(seq2seq2seq_sess, tf.train.latest_checkpoint(seq2seq_rnn.path_to_model))
+seq2seq_sess = tf.Session()
+seq2seq_saver = tf.train.Saver()
+saver.restore(seq2seq_sess, tf.train.latest_checkpoint(seq2seq_rnn.path_to_model))
 
 print("loading saved ngram")
 ngram_model = ngram.NGRAM_model("./ngrams/models")
