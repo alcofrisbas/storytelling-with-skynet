194bae6069de5d3f5417f2e26ecdc8ef6c729f3f
alcofrisbas <bgreene231@gmail.com>
2019-03-02 22:12:03 -0600

    changed rnn_train args

--- writerbot-webapp/webapp/views.py
+++ writerbot-webapp/webapp/views.py
@@ -42,7 +42,7 @@ class PromptMode(Enum):
     DICKENS = 2
     NONE = 3
 
-args_dict = {"n_input": 10, "batch_size": 1, "n_hidden": 300, "learning_rate": 0.001, "training_iters": 50000, "training_file": "simpleRNN/data/train.txt"}
+args_dict = {"n_input": 20, "batch_size": 1, "n_hidden": 300, "learning_rate": 0.001, "training_iters": 50000, "training_file": "simpleRNN/data/train.txt"}
 display_step = 1000
 path_to_model = config("PATH_TO_RNN")#"simpleRNN/models/"
 path_to_seq2seq_model = config("PATH_TO_SEQ")#"simpleRNN/seq2seq_models/"
@@ -260,8 +260,6 @@ def generatePrompt(prompt_mode):
 
 
 def generateSuggestion(session, newSentence, mode):
-    print(int(mode))
-    print(newSentence)
     try:
         if int(mode) == Mode.RNN.value:
             suggestion = rnn.generate_suggestion(session, newSentence)
