fixed silent failing on part of ngram loading

writerbot-webapp/webapp/views.py
@@ -22,8 +22,7 @@ import tensorflow as tf
 import random
 from webapp.words import ADJECTIVES, ANIMALS
 
-# little hacky shit to make pickling work for loading the ngram model
-# fastly.
+# little hacky shit to make pickling work for loading the ngram model fastly.
 sys.path.append(os.path.join(os.path.dirname(sys.path[0]),'./'))
 from ngrams import ngram
 sys.modules["ngram"] = ngram
@@ -46,31 +45,33 @@ args_dict = {"n_input": 4, "batch_size": 1, "n_hidden": 300, "learning_rate": 0.
 display_step = 1000
 path_to_model = config("PATH_TO_RNN")#"simpleRNN/models/"
 path_to_seq2seq_model = config("PATH_TO_SEQ")#"simpleRNN/seq2seq_models/"
-model_name = config("RNN_MODEL_NAME")#"basic_model"
+rnn_model_name = config("RNN_MODEL_NAME")#"basic_model"
+s2s_model_name = config("SEQ_MODEL_NAME")
 print("instantiating RNN")
 sess = tf.Session()
-rnn = SimpleRNN(args_dict, display_step, path_to_model, model_name)
+rnn = SimpleRNN(args_dict, display_step, path_to_model, rnn_model_name)
 print("instantiating saver")
 saver = tf.train.Saver()
 print("loading saved RNN from " + rnn.path_to_model)
 saver.restore(sess, tf.train.latest_checkpoint(rnn.path_to_model))
 
-with tf.Graph().as_default():
-    seq2seq_sess = tf.Session()
-    #with tf.variable_scope("seq2seq"):
-    seq2seq_rnn = seq2seqRNN(args_dict, display_step, path_to_seq2seq_model, model_name, False)
-    print("loading saved seq2seqRNN from " + seq2seq_rnn.path_to_model)
-    seq2seq_saver = tf.train.Saver()
-    seq2seq_saver.restore(seq2seq_sess, tf.train.latest_checkpoint(seq2seq_rnn.path_to_model))
+# with tf.Graph().as_default():
+#     seq2seq_sess = tf.Session()
+#     #with tf.variable_scope("seq2seq"):
+#     seq2seq_rnn = seq2seqRNN(args_dict, display_step, path_to_seq2seq_model, s2s_model_name, False)
+#     print("loading saved seq2seqRNN from " + seq2seq_rnn.path_to_model)
+#     seq2seq_saver = tf.train.Saver()
+#     seq2seq_saver.restore(seq2seq_sess, tf.train.latest_checkpoint(seq2seq_rnn.path_to_model))
 
 print("loading saved ngram")
 ngram_model = ngram.NGRAM_model("./ngrams/models")
 prompt_model = ngram.NGRAM_model("./ngrams/models")
-ngram_model.create_model("5max200000.model")
-#ngram_model.create_model("dickens_model")
-#ngram_model.set_model("lewis_model2")
-prompt_model.create_model("5max200000.model")
-prompt_model.set_model("5max200000.model")
+ngram_model.create_model("dickens_model")
+ngram_model.set_model("dickens_model")
+
+prompt_model.create_model("dickens_model")
+prompt_model.create_model("lewis_model2")
+prompt_model.set_model("dickens_model")
 ngram_model.m = 2
 ngram_model.high = 100
 prompt_model.m = 2
@@ -257,11 +258,13 @@ def generatePrompt(prompt_mode):
 
 
 def generateSuggestion(session, newSentence, mode):
+    print(int(mode))
+    print(newSentence)
     try:
         if int(mode) == Mode.RNN.value:
             suggestion = rnn.generate_suggestion(session, newSentence)
         elif int(mode) == Mode.SEQ2SEQ.value:
-            suggestion = "Seq2Seq model not integrated yet."
+            suggestion = "asdf"#suggestion = seq2seq.generate_suggestion(session, newSentence)
         elif int(mode) == Mode.NGRAM.value:
             suggestion = ngram_model.generate_with_constraints(newSentence)
         elif int(mode) == Mode.NONE.value: